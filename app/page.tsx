26d007422507aabe87f0ae48b47471088412d5b2
benbassignot-byte <ben.bassignot@gmail.com>
2026-09-17 15:57:00 +0200

    Fix product color naming and order

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const products = [
+  { name: "Noir / rouge", images: ["IMG_8687.jpg", "IMG_8688.jpg"] },
   { name: "Rouge / noir", images: ["IMG_5983.JPG", "IMG_5984.JPG"] },
   { name: "Rouge / blanc", images: ["IMG_5991.JPG", "IMG_5992.JPG"] },
-  { name: "Noir / rouge", images: ["IMG_8687.jpg", "IMG_8688.jpg"] },
   {
     name: "Noir / violet",
     tag: "MODÈLE COMPACT",
