d149991b9021ccea4a82f8a10e251ebb84f6db61
benbassignot-byte <ben.bassignot@gmail.com>
2026-09-17 15:59:55 +0200

    Fix product image placement order

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const products = [
-  { name: "Noir / rouge", images: ["IMG_8687.jpg", "IMG_8688.jpg"] },
-  { name: "Rouge / noir", images: ["IMG_5983.JPG", "IMG_5984.JPG"] },
+  { name: "Noir / rouge", images: ["IMG_5983.JPG", "IMG_5984.JPG"] },
+  { name: "Rouge / noir", images: ["IMG_8687.jpg", "IMG_8688.jpg"] },
   { name: "Rouge / blanc", images: ["IMG_5991.JPG", "IMG_5992.JPG"] },
   {
     name: "Noir / violet",
